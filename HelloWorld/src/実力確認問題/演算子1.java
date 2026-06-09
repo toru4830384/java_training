360a2bb905ea4b96789896b480b51bead8e1b49e
nigg <nigg@localhost>
2026-06-09 15:57:39 +0900

    演算子1~4

--- HelloWorld/src/実力確認問題/演算子1.java
+++ HelloWorld/src/実力確認問題/演算子1.java
@@ -1,7 +1,9 @@
 package 実力確認問題;
 
  public class 演算子1 {
-	 
+	 /**
+	  * "Hello World"を出力します。
+	  */
 	 public static void main(String[] args) {
 		 
 		 System.out.println("Hello World!");
--- HelloWorld/src/実力確認問題/演算子2.java
+++ HelloWorld/src/実力確認問題/演算子2.java
@@ -3,7 +3,9 @@ package 実力確認問題;
  public class 演算子2 {
 	 
 	 public static void main(String[] args) {
-		 
+		 /**
+		  * str1・2に代入した文字を結合し出力します。
+		  */
 		 String str1 = "Hello";
 		 String str2 = "World!";
 		 
