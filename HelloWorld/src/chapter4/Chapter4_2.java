28caebf0f6948a4adb706ff30a089707d4e7561c
nigg <nigg@localhost>
2026-06-15 15:17:19 +0900

    実力確認問題４章

---
+++ HelloWorld/src/chapter4/Chapter4_2.java
@@ -0,0 +1,15 @@
+package chapter4;
+
+import java.io.*;
+
+public class Chapter4_2 {
+
+    public static void main(String[] args) throws IOException 
+      {
+    	
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+        
+        String[][] products = new String[3][3];
+      }
+}
