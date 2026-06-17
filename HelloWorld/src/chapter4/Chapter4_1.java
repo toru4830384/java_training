18223b4c0af8c8185d5a8cf235abedcdc0e0aca0
nigg <nigg@localhost>
2026-06-17 15:55:48 +0900

    4章

--- HelloWorld/src/chapter4/Chapter4_1.java
+++ HelloWorld/src/chapter4/Chapter4_1.java
@@ -1,36 +1,35 @@
-package chapter4; 	
+package chapter4;
+
 /**
- * 契約者情報を表示して
- * 遅延利息金が発生している人を数えるプログラムです。
+ * 契約者情報を表示して 遅延利息金が発生している人を数えるプログラムです。
  */
- public class Chapter4_1 {											
-	  /**
-	   * 契約者名と保険料を表示して
-	   * 遅延利息金が発生している人数を出力します。
-	   * 
-	   * @param args プログラム起動時の引数
-	   */
-	  public static void main(String[] args) {
-		  
-			String[][] contracts = {
-					{"中村","5000","2000"},
-					{"田島","3000",""},
-					{"小沢","4500",""},
-					{"大西","6000","3500"}					
-			};
-			
-			int count = 0;
-			
-			for(int i=0; i<contracts.length; i++) {
-				System.out.println("契約者名:" + contracts[i][0] + 
-						         "、保険料:" + contracts[i][1] + "円");
-				
-			
-			   if (!contracts[i][2].equals("")) {
-				   count++;
-			   }
+public class Chapter4_1 {
+	/**
+	 * 契約者名と保険料を表示して 遅延利息金が発生している人数を出力します。
+	 * 
+	 * @param args プログラム起動時の引数
+	 */
+	public static void main(String[] args) {
+
+		String[][] contracts = { 
+				{ "中村", "5000", "2000" }, 
+				{ "田島", "3000", "" }, 
+				{ "小澤", "4500", "" },
+				{ "大西", "6000", "3500" } 
+	    };
+
+		int count = 0;
+
+		for (int i = 0; i < contracts.length; i++) {
+			System.out.println("契約者名:" + contracts[i][0] 
+					+ "、保険料:" + contracts[i][1] + "円");
+
+			if (!contracts[i][2].equals("")) {
+				count++;
 			}
-			
-			System.out.println("遅延利息金が発生している人数は" + count + "人です。");		
-	  }
- }
+		}
+
+		System.out.println("遅延利息金が発生している人数は" 
+		                   + count + "人です。");
+	}
+}
--- HelloWorld/src/chapter4/Chapter4_2.java
+++ HelloWorld/src/chapter4/Chapter4_2.java
@@ -4,12 +4,23 @@ import java.io.*;
 
 public class Chapter4_2 {
 
-    public static void main(String[] args) throws IOException 
-      {
-    	
-        BufferedReader br =
-                new BufferedReader(new InputStreamReader(System.in));
-        
-        String[][] products = new String[3][3];
-      }
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = 
+				new BufferedReader(new InputStreamReader(System.in));
+
+		String[][] products = new String[3][3];
+
+		for (int i = 0; i < products.length; i++) {
+
+			System.out.println("商品を入力してください。");
+			products[i][0] = br.readLine();
+
+			System.out.println("単価を入力してください。");
+			products[i][1] = br.readLine();
+
+			System.out.println("支払金額を入力してください。");
+			products[i][2] = br.readLine();
+		}
+	}
 }
